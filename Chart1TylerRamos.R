dd6d00f7eb8923838edc2bd03b5c508fc0e73c47
merp206 <tyler.merp@Tylers-MacBook-Air-3.local>
2023-02-28 15:36:51 -0800

    A3 v5

--- Chart1TylerRamos.R
+++ Chart1TylerRamos.R
@@ -3,10 +3,9 @@ library(dplyr)
 SPL_df <- read.csv("2013-2023-5-Checkouts-SPL.csv")
 SPL_df_subset_usage <- SPL_df %>% select(UsageClass, CheckoutYear, Checkouts)
 SPL_df_grouped_usage <- SPL_df_subset_usage %>% group_by(UsageClass, CheckoutYear) %>% summarize(tot = sum(Checkouts))
-graph1 <- ggplot(data = SPL_df_grouped_usage) +
+ggplot(data = SPL_df_grouped_usage) +
   geom_line(mapping = aes(y = tot, x = CheckoutYear, color = UsageClass)) +
   scale_x_continuous(name = "Year", breaks = seq(2013, 2023, 1)) + 
   scale_y_continuous(name = "Number Of Checkouts", breaks = seq(0, 5500000, 500000)) +
   labs(title = 'Digital Media vs Physical Media', color = 'Content Type') +
   theme(plot.title = element_text(color = "black", size = 14))
-graph1
