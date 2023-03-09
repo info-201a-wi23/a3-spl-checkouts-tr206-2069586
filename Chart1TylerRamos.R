3d3573ec889079a62df7c82f6524861b700ee8f5
merp206 <tyler.merp@Tylers-MacBook-Air-3.local>
2023-03-09 12:22:58 -0800

    resubit

--- Chart1TylerRamos.R
+++ Chart1TylerRamos.R
@@ -1,11 +1,12 @@
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
 SPL_df <- read.csv("2013-2023-5-Checkouts-SPL.csv")
 SPL_df_subset_usage <- SPL_df %>% select(UsageClass, CheckoutYear, Checkouts)
 SPL_df_grouped_usage <- SPL_df_subset_usage %>% group_by(UsageClass, CheckoutYear) %>% summarize(tot = sum(Checkouts))
-ggplot(data = SPL_df_grouped_usage) +
-  geom_line(mapping = aes(y = tot, x = CheckoutYear, color = UsageClass)) +
+ggplot(data = SPL_df_grouped_usage) + 
+  geom_line(mapping = aes(y = tot, x = CheckoutYear, color = UsageClass)) + 
   scale_x_continuous(name = "Year", breaks = seq(2013, 2023, 1)) + 
-  scale_y_continuous(name = "Number Of Checkouts", breaks = seq(0, 5500000, 500000)) +
-  labs(title = 'Digital Media vs Physical Media', color = 'Content Type') +
+  scale_y_continuous(name = "Number Of Checkouts", breaks = seq(0, 5500000, 500000)) + 
+  labs(title = 'Digital Media vs Physical Media', color = 'Content Type') + 
   theme(plot.title = element_text(color = "black", size = 14))
--- Chart3TylerRamos.R
+++ Chart3TylerRamos.R
@@ -1,6 +1,6 @@
-# Do month vs how many checkouts to see if there is a day of the week which is most popular
-
 library(ggplot2)
+library(dplyr)
+library(tidyverse)
 SPL_df <- read.csv("2013-2023-5-Checkouts-SPL.csv")
 
 SPL_df_subset_month <- SPL_df %>% select(CheckoutMonth, Checkouts)
@@ -27,5 +27,6 @@ graph3 <-
   geom_bar(stat="identity") + 
   scale_y_continuous(name = "Number Of Checkouts", breaks = seq(0, 5000000, 500000)) +
   labs(title = 'Total checkouts by Month', x = "Month") +
+  coord_flip() +
   theme(plot.title = element_text(color = "black", size = 30))
 graph3
